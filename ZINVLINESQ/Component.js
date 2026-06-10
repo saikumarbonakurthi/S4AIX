fa2acf4c7ab301276016f11dca8278ef02d3ca87
Sai Kumar Bonakurthi <saikumarbonakurthi@gmail.com>
2026-06-09 20:18:17 -0400

    PDF
    Commit

---
+++ ZINVLINESQ/Component.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+    "sap/suite/ui/generic/template/lib/AppComponent"
+], function (AppComponent) {
+    "use strict";
+
+    return AppComponent.extend("LineItemSuperQuery.Component", {
+        metadata: {
+            manifest: "json"
+        }
+    });
+});
